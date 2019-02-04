802991476d16edc12a80478907ae4e3bf15e25e2
Pawel <paweljozwiak1986@gmail.com>
2019-02-04 12:26:30 +0100

    Grabbing input and buttons

---
+++ app/TodoView.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", function(event) {
+ 
+    const addTodoInput = document.querySelector('#todo-input'),
+    addTodoBtn = document.querySelector('#add-todo-btn');
+
+
+ 
+});
