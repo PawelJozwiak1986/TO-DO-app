72230636650d52c39cd109855efcc2def7404f7a
Pawel <paweljozwiak1986@gmail.com>
2019-02-04 12:27:44 +0100

    EventListener to btn

--- app/TodoView.js
+++ app/TodoView.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const addTodoInput = document.querySelector('#todo-input'),
     addTodoBtn = document.querySelector('#add-todo-btn');
 
-
+        
+    addTodoBtn.addEventListener('click', function (event) {
+        const inputValue = addTodoInput.value;
+        alert(inputValue);
+    });
  
 });
